262a771a268c5e7d4fb24d06c0560580b7114159
Denny <den.shel@inbox.ru>
2022-09-23 23:30:40 +0300

    r1.1:mongoDB

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+mongoose
+  .connect(
+    'mongodb+srv://blogUser:ererer@cluster0.wrwomv7.mongodb.net/?retryWrites=true&w=majority'
+  )
+  .then(() => console.log('Connection to DB - OK!'))
+  .catch((err) => console.log('DB error', err));
 
 const app = express();
 
